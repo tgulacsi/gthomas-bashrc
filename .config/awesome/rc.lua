d59e02c84fba12d4e3bae40128f8330805be2a60
Tamás Gulácsi <gt-uno-tgulacsi-desktop@gthomas.homelinux.org>
2011-05-17 15:54:52 +0200

    add doctorx-aws.pem

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -244,9 +244,7 @@ awful.key({ modkey, "Shift" }, "F12",
 awful.key({ modkey, "Control" }, "F12",
       function () awful.util.spawn("sudo /usr/sbin/pm-hibernate") end),
 awful.key({ modkey }, "F12",
-      function () awful.util.spawn("xlock") end),
-awful.key({ modkey }, "E",
-      function () awful.util.spawn("pcmanfm") end),
+      function () awful.util.spawn("nice -n 15 xlock") end),
 awful.key({ modkey, "Shift" }, "f",
      function () awful.util.spawn("x-www-browser") end)
 
