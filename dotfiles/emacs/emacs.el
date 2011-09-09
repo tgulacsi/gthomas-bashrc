168c55cb884d6249956a5edb7ed0619b602ed213
Tamás Gulácsi <gt-uno-tgulacsi-desktop@gthomas.homelinux.org>
2011-09-09 13:32:28 +0200

    emacs.el mod

--- dotfiles/emacs/emacs.el
+++ dotfiles/emacs/emacs.el
@@ -7,7 +7,20 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(font-use-system-font t))
+ '(column-number-mode t)
+ '(evil-mode t)
+ '(font-use-system-font t)
+ '(global-hl-line-mode t)
+ '(global-linum-mode t)
+ '(global-whitespace-mode t)
+ '(inhibit-startup-screen t)
+ '(major-mode (quote evil-local-mode))
+ '(menu-bar-mode t)
+ '(python-check-command "flake8")
+ '(server-mode t)
+ '(show-paren-mode t)
+ '(whitespace-action nil)
+ '(whitespace-global-modes (quote (not))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
