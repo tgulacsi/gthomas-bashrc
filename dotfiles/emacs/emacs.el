3805fbde3bfc57af60352beda490b78c9d43ed9c
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2011-12-12 15:38:04 +0100

    bump emacs.el

--- dotfiles/emacs/emacs.el
+++ dotfiles/emacs/emacs.el
@@ -9,7 +9,7 @@
 ;; solarized
 (add-to-list 'load-path "~/.emacs.d/external/solarized")
 ;(add-to-list 'custom-theme-load-path "~/.emacs.d/external/solarized")
-;(require 'solarized-dark-theme)
+;require 'solarized-dark-theme)
 (require 'solarized-light-theme)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -23,4 +23,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Inconsolata")))))
+;'(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Inconsolata")))))
