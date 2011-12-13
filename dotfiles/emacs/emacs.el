fb54aa68266a9d4cd47e4673c07bc53b19dff834
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2011-12-13 08:33:14 +0100

    .emacs: dish solarized, welcome wombat!

--- dotfiles/emacs/emacs.el
+++ dotfiles/emacs/emacs.el
@@ -7,9 +7,9 @@
 (require 'evil)
 (evil-mode 1)
 ;; solarized
-(add-to-list 'load-path "~/.emacs.d/external/solarized")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/external/solarized")
-(require 'solarized-dark-theme)
+;(add-to-list 'load-path "~/.emacs.d/external/solarized")
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/external/solarized")
+;(require 'solarized-dark-theme)
 ;(require 'solarized-light-theme)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -21,15 +21,3 @@
  '(custom-enabled-themes (quote (wombat)))
  '(font-use-system-font t)
  '(visible-bell t))
-;(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-;'(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Inconsolata")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
