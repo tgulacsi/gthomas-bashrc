39df2587d711720dc75bf08cbb1ba62cc46f4dc2
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2013-02-07 14:46:26 +0100

    update unots and uno-splprn for the new syntax

--- bin/screens.sh
+++ bin/screens.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-N=$(xrandr | grep ' connected' | wc -l)
+N=${1:-$(xrandr | grep ' connected' | wc -l)}
 case "${N:-1}" in
-  1) xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output VGA1 --off ;;
-  2) xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output VGA1 --off ;;
+  1) CMD='xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output VGA1 --off' ;;
+  2) CMD='xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output VGA1 --off' ;;
   *) echo "unknown number of connected outputs: $N >&2" ; exit $N ;;
 esac
+echo $CMD
+exec $CMD
 
