1ef9508a7b092bff6b305027e3f6e1a22f411a20
Tamás Gulácsi <tgulacsi78@gmail.com>
2013-03-07 20:40:08 +0100

    add .emacs.d

---
+++ dotfiles/.emacs.d/elpa/pomodoro-20130116.1511/pomodoro-autoloads.el
@@ -0,0 +1,28 @@
+;;; pomodoro-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (pomodoro-start) "pomodoro" "pomodoro.el" (20792
+;;;;;;  60446))
+;;; Generated autoloads from pomodoro.el
+
+(autoload 'pomodoro-start "pomodoro" "\
+
+
+\(fn ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pomodoro-pkg.el") (20792 60446 181822))
+
+;;;***
+
+(provide 'pomodoro-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pomodoro-autoloads.el ends here
---
+++ dotfiles/.emacs.d/elpa/pomodoro-20130116.1511/pomodoro-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pomodoro" "20130116.1511" "A timer for the Pomodoro Technique [source: github]" (quote nil))
