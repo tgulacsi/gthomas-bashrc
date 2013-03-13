ea79fa8faebb7b967243c02fc0b8822bd8634abc
Tamás Gulácsi <tgulacsi78@gmail.com>
2013-03-13 20:27:39 +0100

    zshrc: rm no -i !

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -8,7 +8,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(tab-width 4))
+ '(tab-width 4)
+ '(vc-suppress-confirm t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
