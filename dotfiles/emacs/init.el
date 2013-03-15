a2514f9682961bf708c4d3c125ee91a74821acf0
Tamás Gulácsi <tgulacsi78@gmail.com>
2013-03-15 08:06:25 +0100

    emacs: safe theme

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -8,6 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(tab-width 4)
  '(vc-suppress-confirm t))
 (custom-set-faces
