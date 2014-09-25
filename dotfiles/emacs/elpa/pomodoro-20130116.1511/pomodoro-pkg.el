1df271c6843b57f5f443a00eb9a436d87a3898e5
Tamás Gulácsi <tgulacsi78@gmail.com>
2014-09-25 21:02:54 +0200

    emacs

--- dotfiles/emacs/elpa/pomodoro-20130116.1511/pomodoro-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pomodoro" "20130116.1511" "A timer for the Pomodoro Technique [source: github]" (quote nil))
--- dotfiles/emacs/elpa/pyflakes-1.0/pyflakes-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; pyflakes-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (pyflakes) "pyflakes" "pyflakes.el" (20792 60522))
-;;; Generated autoloads from pyflakes.el
-
-(autoload 'pyflakes "pyflakes" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("pyflakes-pkg.el") (20792 60522 895924))
-
-;;;***
-
-(provide 'pyflakes-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; pyflakes-autoloads.el ends here
