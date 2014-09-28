4b2763e7b1b22f054bfe54d40aeee1f3e84702e9
Tamás Gulácsi <tgulacsi78@gmail.com>
2014-09-28 17:59:55 +0200

    fix xsession

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -85,6 +85,20 @@
 
 (add-hook 'go-mode-hook (lambda ()
                          (fci-mode)
+			 (setq indent-tabs-mode t)
+			 (setq tab-width 4)
                          (set-fill-column 78)))
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tab-width 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
