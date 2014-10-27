ed0f002fa00136194e50ccb883d3b2c390469f38
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2014-10-27 09:15:35 +0100

    screens.sh

--- bin/screens.sh
+++ bin/screens.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-N=${1:-$(xrandr | grep ' connected' | wc -l)}
+connected=$(xrandr | grep -F ' connected')
+N=${1:-$(echo "$connected" | wc -l)}
+main=$(echo "$connected" | cut -d\  -f1,3 | grep -v -F HDMI1)
+nm=${main% *}
+sz=${main#* }
+sz=${sz%%+*}
+pos=${sz%x*}
 case "${N:-1}" in
-  1) CMD='xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output VGA1 --off' ;;
-  2) CMD='xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output VGA1 --off' ;;
+  1) CMD="xrandr --output HDMI1 --off --output $nm --mode $sz --pos 0x0 --rotate normal" ;;
+  2) CMD="xrandr --output HDMI1 --mode 1920x1080 --pos ${pos}x0 --rotate normal --output $nm --mode $sz --pos 0x0 --rotate normal" ;;
   *) echo "unknown number of connected outputs: $N >&2" ; exit $N ;;
 esac
 echo $CMD
