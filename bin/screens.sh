7f0de7a6f7d536828a82a15c6905e0792987bc85
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2014-11-19 10:46:28 +0100

    add scale 0.75

--- bin/screens.sh
+++ bin/screens.sh
@@ -5,10 +5,15 @@ main=$(echo "$connected" | cut -d\  -f1,3 | grep -v -F HDMI1)
 nm=${main% *}
 sz=${main#* }
 sz=${sz%%+*}
+sz=${SIZE:-1920x1080}
 pos=${sz%x*}
+scale=
+if [ ${pos} -gt 1366 ]; then
+    scale=--scale 0.75x0.75
+fi
 case "${N:-1}" in
-  1) CMD="xrandr --output HDMI1 --off --output $nm --mode 1366x768 --pos 0x0 --rotate normal" ;;
-  2) CMD="xrandr --output HDMI1 --mode 1366x768 --pos ${pos}x0 --rotate normal --output $nm --mode $sz --pos 0x0 --rotate normal" ;;
+  1) CMD="xrandr --output HDMI1 --off --output $nm --mode $sz $scale --pos 0x0 --rotate normal" ;;
+  2) CMD="xrandr --output HDMI1 --mode $sz --pos ${pos}x0 --rotate normal --output $nm --mode $sz $scale --pos 0x0 --rotate normal" ;;
   *) echo "unknown number of connected outputs: $N >&2" ; exit $N ;;
 esac
 echo $CMD
