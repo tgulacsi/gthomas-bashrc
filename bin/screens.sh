5f5e1361e5e16abb3c25556e2b9ec77268a91d1c
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2014-11-19 10:11:03 +0100

    screens.sh: 1366x768

--- bin/screens.sh
+++ bin/screens.sh
@@ -7,8 +7,8 @@ sz=${main#* }
 sz=${sz%%+*}
 pos=${sz%x*}
 case "${N:-1}" in
-  1) CMD="xrandr --output HDMI1 --off --output $nm --mode $sz --pos 0x0 --rotate normal" ;;
-  2) CMD="xrandr --output HDMI1 --mode 1920x1080 --pos ${pos}x0 --rotate normal --output $nm --mode $sz --pos 0x0 --rotate normal" ;;
+  1) CMD="xrandr --output HDMI1 --off --output $nm --mode 1366x768 --pos 0x0 --rotate normal" ;;
+  2) CMD="xrandr --output HDMI1 --mode 1366x768 --pos ${pos}x0 --rotate normal --output $nm --mode $sz --pos 0x0 --rotate normal" ;;
   *) echo "unknown number of connected outputs: $N >&2" ; exit $N ;;
 esac
 echo $CMD
