89c51b92921d180677ddcb9a7ac556f547c68971
Tamás Gulácsi <tgulacsi78@gmail.com>
2017-07-19 13:12:14 +0200

    screens: head -n1

--- bin/screens.sh
+++ bin/screens.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-connected=$(xrandr | grep -F ' connected')
+connected=$(xrandr | grep -F ' connected' | fgrep -v 'not connected')
 N=${1:-$(echo "$connected" | wc -l)}
 main=$(echo "$connected" | cut -d\  -f1,3 | grep -v -F HDMI)
 nm=${main% *}
@@ -9,11 +9,14 @@ sz=${sz%%+*}
 sz=${SIZE:-1920x1080}
 pos=${sz%x*}
 scale=
-if [ ${pos} -gt 1366 ]; then
+noScale=$(xrandr | sed -ne '/DP-1 connected/,/connected/p' | fgrep -q '1920x1080')
+
+if [ $noScale -eq 0 ] && [ ${pos} -gt 1366 ]; then
     scale='--scale 0.75x0.75'
 fi
-NAME=$(xrandr | fgrep -v VIRTUAL | fgrep connected | cut -d\  -f1 | fgrep -v eDP)
+NAME=$(xrandr | fgrep -v VIRTUAL | fgrep connected | fgrep -v 'not connected' | cut -d\  -f1 | fgrep -v eDP | head -n 1)
 NAME=${NAME:-HDMI1}
+echo "NAME=$NAME"
 case "${N:-1}" in
   1) CMD="xrandr --output $NAME --off --output $nm --mode $sz $scale --pos 0x0 --rotate normal" ;;
   2) CMD="xrandr --output $NAME --mode $sz --pos ${pos}x0 --rotate normal --output $nm --mode $sz $scale --pos 0x0 --rotate normal" ;;
