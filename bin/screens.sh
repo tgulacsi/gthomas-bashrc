9830bd2fc7b0d68a8acc089407b47d6ba69e451a
Tamás Gulácsi <tgulacsi78@gmail.com>
2018-01-10 17:19:05 +0100

    screens: escape xrandr output

--- bin/screens.sh
+++ bin/screens.sh
@@ -10,18 +10,17 @@ sz=${SIZE:-1920x1080}
 pos=${sz%x*}
 
 scale=1x1
-noScale=$(xrandr | sed -ne '/DP-1 connected/,/connected/p' | fgrep -q '1920x1080')
+noScale="$(xrandr | sed -ne '/DP-1 connected/,/connected/p' | fgrep -q '1920x1080')"
 if [ -n "$noScale" ] && [ "$noScale" -eq 1 ] && [ "${pos}" -ge 1367 ]; then
-    scale='--scale 0.7114583x0.7114583'
+    scale='0.7114583x0.7114583'
 fi
-NAME=$(xrandr | fgrep -v VIRTUAL | fgrep connected | fgrep -v 'not connected' | cut -d\  -f1 | fgrep -v eDP | head -n 1)
+NAME="$(xrandr | grep -Fv VIRTUAL | grep -F connected | fgrep -Fv 'not connected' | cut -d\  -f1 | grep -Fv eDP | head -n 1)"
 NAME=${NAME:-HDMI1}
 echo "NAME=$NAME"
 case "${N:-1}" in
-  1) CMD="xrandr --output $NAME --off --output $nm --mode $sz $scale --pos 0x0 --rotate normal" ;;
-  2) CMD="xrandr --output $NAME --mode $sz --pos ${pos}x0 --rotate normal --output $nm --mode $sz $scale --pos 0x0 --rotate normal" ;;
-  *) echo "unknown number of connected outputs: $N >&2" ; exit $N ;;
+  1) CMD="xrandr --output $NAME --off --output '$nm' --mode $sz --scale $scale --pos 0x0 --rotate normal" ;;
+  2) CMD="xrandr --output $NAME --mode $sz --pos ${pos}x0 --rotate normal --output $nm --mode $sz --scale $scale --pos 0x0 --rotate normal" ;;
+  *) echo "unknown number of connected outputs: $N >&2" ; exit "$N" ;;
 esac
-echo $CMD
+echo "$CMD"
 exec $CMD
-
