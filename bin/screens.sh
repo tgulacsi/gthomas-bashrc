619750a5c808bf86c4f87acd632e628dfe832aee
Tamás Gulácsi <tgulacsi78@gmail.com>
2018-10-15 11:38:43 +0200

    screen.sh: pos from DP-1

--- bin/screens.sh
+++ bin/screens.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -x
 connected=$(xrandr | grep -F ' connected' | fgrep -v 'not connected')
 N=${1:-$(echo "$connected" | wc -l)}
 main=$(echo "$connected" | cut -d\  -f1,3 | grep -v -F HDMI)
@@ -10,19 +9,16 @@ if [ -z "$SIZE" ]; then
 	SIZE="$(xrandr | sed -ne "/^$NAME/,/[*]/p" | grep -F '*' | tail -n1 | awk '{print $1}')"
 fi
 nm=${main% *}
-sz=${main#* }
-sz=${sz%%+*}
-sz=${SIZE:-}
-pos=${sz%x*}
+pos="$(xrandr | sed -ne '/DP-1 connected/ s/^.* \([1-9][0-9]*\)x.*$/\1/p')"
+echo "# main=$main SIZE=$SIZE -> nm=$nm pos=$pos" >&2
 
 scale=1x1
-noScale="$(xrandr | sed -ne '/DP-1 connected/,/connected/p' | grep -Fq '1920x1080')"
-if [ -n "$noScale" ] && [ "$noScale" -eq 1 ] && [ "${pos}" -ge 1367 ]; then
-    scale='0.7114583x0.7114583'
+if [ "${pos}" -ge 1367 ]; then
+    scale='0.711x0.711'
 fi
 case "${N:-1}" in
   1) CMD="xrandr --output $NAME --auto" ;;
-  2) CMD="xrandr --output $NAME --auto --pos ${pos}x0 --rotate normal --output $nm --auto --pos 0x0 --rotate normal" ;;
+  2) CMD="xrandr --output $NAME --auto --pos ${pos}x0 --rotate normal --scale 1x1 --output $nm --auto --pos 0x0 --rotate normal" ;;
   *) echo "unknown number of connected outputs: $N >&2" ; exit "$N" ;;
 esac
 echo "$CMD"
