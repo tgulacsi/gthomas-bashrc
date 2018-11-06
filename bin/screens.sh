5991581fadc68c127b7bc836c811622a539b00ba
Tamás Gulácsi <tgulacsi78@gmail.com>
2018-11-06 20:34:46 +0100

    screens.sh: quote variables

--- bin/screens.sh
+++ bin/screens.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 set -u
-connected=$(xrandr | grep -F ' connected' | fgrep -v 'not connected')
+connected="$(xrandr | grep -F ' connected' | grep -Fv 'not connected')"
 N=${1:-$(echo "$connected" | wc -l)}
 main=$(echo "$connected" | cut -d\  -f1 | grep -v -F HDMI)
 NAME="$(xrandr | grep -Fv VIRTUAL | grep -F connected | grep -Fv 'not connected' | cut -d\  -f1 | grep -Fv eDP | head -n 1)"
@@ -14,19 +14,15 @@ nm=${main% *}
 pos="$(xrandr | sed -ne '/DP-1 connected/,/+/ s/^.* \([1-9][0-9]*\)x.*$/\1/p' | head -n1)"
 echo "# main=$main SIZE=$SIZE -> nm=$nm pos=$pos" >&2
 
-scale=1x1
-if [ "${pos}" -ge 1367 ]; then
-    scale='0.711x0.711'
-fi
 set -x
 case "${N:-1}" in
-  1) exec xrandr --output $main --auto
+  1) exec xrandr --output "$main" --auto
 	  ;;
   2)
-	  if [ ${SIZE%x*} -ge 2560 ]; then
-		exec xrandr --output $main --off --output $NAME --auto --pos ${pos}x0 --rotate normal --scale 1x1
+	  if [ "${SIZE%x*}" -ge 2560 ]; then
+		exec xrandr --output "$main" --off --output "$NAME" --auto --pos "${pos}x0" --rotate normal --scale 1x1
 	  else
-		exec xrandr --output $nm --auto --pos 0x0 --rotate normal --output $NAME --auto --pos ${pos}x0 --rotate normal --scale 1x1
+		exec xrandr --output "$nm" --auto --pos 0x0 --rotate normal --output "$NAME" --auto --pos "${pos}x0" --rotate normal --scale 1x1
 	  fi
 	  ;;
   *) echo "unknown number of connected outputs: $N >&2" ;
