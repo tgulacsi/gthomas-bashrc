320e8290aedcd7c62cd00739331b702d1cf0fd13
Tamás Gulácsi <tgulacsi78@gmail.com>
2018-11-09 08:03:47 +0100

    screens.h: allow DP-1-1

--- bin/screens.sh
+++ bin/screens.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 set -e
 set -u
-connected="$(xrandr | grep -F ' connected' | grep -Fv 'not connected')"
+connected="$(xrandr | grep -F ' connected' | grep -Fv 'not connected' | cut -d\  -f1)"
+echo "# connected=$connected" >&2
 N=${1:-$(echo "$connected" | wc -l)}
-main=$(echo "$connected" | cut -d\  -f1 | grep -v -F HDMI)
-NAME="$(xrandr | grep -Fv VIRTUAL | grep -F connected | grep -Fv 'not connected' | cut -d\  -f1 | grep -Fv eDP | head -n 1)"
+echo "# N=$N" >&2
+if [ "${N:-1}" -eq 1 ]; then
+	exec xrandr --output "$connected" --auto
+elif [ "${N:-1}" -ge 3 ]; then
+	echo "unknown number of connected outputs: $N" >&2
+	exit $N
+fi
+
+main=$(echo "$connected" | cut -d\  -f1 | grep -v -E 'DP-[0-9]-|HDMI')
+echo "# main=$main" >&2
+NAME="$(xrandr | grep -Fv VIRTUAL | grep -F connected | grep -Fv 'disconnected' | cut -d\  -f1 | grep -Fv eDP | head -n 1)"
+echo "# name=$NAME" >&2
 NAME="${NAME:-HDMI-1}"
-echo "NAME=$NAME"
+echo "# NAME=$NAME" >&2
 if [ -z "${SIZE:-}" ]; then
 	SIZE="$(xrandr | sed -ne "/^$NAME/,/[*]/p" | grep -F '*' | tail -n1 | awk '{print $1}')"
 fi
@@ -24,8 +35,5 @@ case "${N:-1}" in
 	  else
 		exec xrandr --output "$nm" --auto --pos 0x0 --rotate normal --output "$NAME" --auto --pos "${pos}x0" --rotate normal --scale 1x1
 	  fi
-	  ;;
-  *) echo "unknown number of connected outputs: $N >&2" ;
-	  exit "$N"
 	  ;;
 esac
