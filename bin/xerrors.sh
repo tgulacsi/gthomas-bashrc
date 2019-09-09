a0cc4a115a120bb4e2dbe3d0f7d1040cf20aa016
Tamás Gulácsi <tgulacsi78@gmail.com>
2019-09-09 09:54:36 +0200

    add kitty and sway

--- bin/xerrors.sh
+++ bin/xerrors.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-sed -i -e '/"github.com\/pkg\/errors"/ s,"github.com/pkg/errors",errors "golang.org/x/xerrors",; /errors.Wrap(/ s/errors.Wrap(\([^,]*\), \([^ ][^)]*\))\()*\)$/errors.Errorf("%s: %w", \2, \1)\3/; /errors.Wrapf(/ s/errors.Wrapf(\([^,]*\), "\([^"]*\)",\(.*\))$/errors.Errorf("\2: %w", \1,\3)/' $(find . -type f -name '*.go')
+sed -i -e '/"github.com\/pkg\/errors"/ s,"github.com/pkg/errors",errors "golang.org/x/xerrors",;
+	/errors.Wrap(/ s/errors.Wrap(\([^,]*\), \([^ ][^)]*\))\()*\)$/errors.Errorf("%s: %w", \2, \1)\3/;
+	/errors.WithMessage(/ s/errors.WithMessage(\([^,]*\), \(.*\))$/errors.Errorf("%s: %w", \2, \1)/;
+	/errors.Wrapf(/ s/errors.Wrapf(\([^,]*\), "\([^"]*\)",\(.*\))$/errors.Errorf("\2: %w", \3,\1)/' \
+	$(find . -type f -name '*.go')
+vi $(grep -Fl errors $(find . -type f -name '*.go'))
