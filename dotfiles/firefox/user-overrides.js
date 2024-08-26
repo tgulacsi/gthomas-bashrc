a80fa2a3f0b038ba4683bc20e4bf2fff7de75ba8
Tamás Gulácsi <tamas@gulacsi.eu>
2024-08-26 09:43:50 +0200

    aerc: gpg-sign

--- dotfiles/firefox/user-overrides.js
+++ dotfiles/firefox/user-overrides.js
@@ -19,11 +19,6 @@ user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore 
 user_pref("privacy.clearOnShutdown.history", false); // 2811 FF127 or lower
 user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811 FF128+
 
-user_pref("dom.security.https_only_mode", false);
-
-user_pref("privacy.resistFingerprinting.letterboxing", false);
-user_pref("letterbox", false);
-user_pref("privacy.window.maxInnerWidth", 3200);
-user_pref("privacy.window.maxInnerHeight", 1800);
-
 user_pref("webgl.disabled", false);
+user_pref("dom.security.https_only_mode", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
